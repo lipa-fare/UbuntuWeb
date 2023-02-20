a7422037f37a7d8f88ca6469991d58b39d18ca82
TanishkReddy07 <tanishkreddy512@gmail.com>
2023-02-20 11:00:45 +0300

    Made UI changes

--- public/app.js
+++ public/app.js
@@ -10,6 +10,25 @@ const contactSection = document.querySelector('.contactSection');
 const aboutBtn = document.querySelector('.aboutBtn');
 const aboutSection = document.querySelector('.aboutSection');
 
+// Show or Hide the small Nav Bar
+const smallNav = document.querySelector('.smallNav');
+const smallNavBtn = document.querySelector('.smallNavBtn');
+const closeBtn = document.querySelector('.closeBtn');
+
+closeBtn.addEventListener('click', (e) => {
+    closeBtn.classList.add('hidden');
+    smallNavBtn.style.display = 'block';
+    smallNav.style.display = "none";
+})
+
+smallNavBtn.addEventListener('click', (e) => {
+    console.log(smallNav);
+    closeBtn.classList.remove('hidden');
+    smallNavBtn.style.display = 'none';
+
+    smallNav.style.display = "block";
+})
+
 aboutBtn.addEventListener('click', (e) => {
     aboutSection.scrollIntoView({behavior: "smooth"})
 })
